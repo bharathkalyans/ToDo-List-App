624bfa5b3517f41a6fbe1f687f05649f9d34fcea
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-06 12:37:47 +0530

    Added a Menu for List Fragment to Search / Sort / Delete and  About Section. And Inflated the Menu XML file in List Fragment Kotlin File.

--- app/src/main/java/com/bharathkalyans/todolist/fragments/list/ListFragment.kt
+++ app/src/main/java/com/bharathkalyans/todolist/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.bharathkalyans.todolist.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.bharathkalyans.todolist.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -28,8 +26,14 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+
+        //Setting Menu
+        setHasOptionsMenu(true)
         return view
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu,menu)
+    }
 }
