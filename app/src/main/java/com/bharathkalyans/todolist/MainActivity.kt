bc0c9a63200ba53696d918addc82e1122d7e6688
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-06 11:18:47 +0530

    Added a navigation bar so that fragments can return back to their nav host fragment activity.

--- app/src/main/java/com/bharathkalyans/todolist/MainActivity.kt
+++ app/src/main/java/com/bharathkalyans/todolist/MainActivity.kt
@@ -2,14 +2,23 @@ package com.bharathkalyans.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
 
 
+    }
 
+    //Navigating back to the Nav Host Fragment.
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+
+        return navController .navigateUp() || super.onSupportNavigateUp()
     }
 }
