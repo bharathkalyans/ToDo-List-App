fc4fcc583fdbf76f655f8c1ea2facd1de4a5cc37
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-07 14:26:52 +0530

    Added Logic to the Add Fragment where User Input is Validated and Added to DataBase.

--- app/src/main/java/com/bharathkalyans/todolist/fragments/add/AddFragment.kt
+++ app/src/main/java/com/bharathkalyans/todolist/fragments/add/AddFragment.kt
@@ -1,13 +1,23 @@
 package com.bharathkalyans.todolist.fragments.add
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.bharathkalyans.todolist.R
+import com.bharathkalyans.todolist.data.models.Priority
+import com.bharathkalyans.todolist.data.models.ToDoData
+import com.bharathkalyans.todolist.data.viewmodel.ToDoViewModel
+import kotlinx.android.synthetic.main.fragment_add.*
 
 
 class AddFragment : Fragment() {
 
+    //View Model
+    private val mToDoViewModel: ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +31,70 @@ class AddFragment : Fragment() {
         return view
     }
 
-
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.add_fragment_menu, menu)
     }
+
+    //This method will be responsible inserting data into the Database!
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.menu_add) {
+            insertDataToDatabase()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertDataToDatabase() {
+
+        val mTitle = title_et.text.toString()
+        val mPriority = priorities_spinner.selectedItem.toString()
+        val mDescription = description_et.text.toString()
+
+        val validation = inputVerifyFromUser(mTitle, mDescription)
+
+        if (validation) {
+            //Inserting data to the Database as Input  user passes the safe check!
+            val newData = ToDoData(
+                0,
+                mTitle,
+                parsePriority(mPriority),
+                mDescription
+            )
+            mToDoViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "Added!", Toast.LENGTH_SHORT).show()
+            //Navigating back to the List Fragment
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Fill Out All Fields!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun parsePriority(priority: String): Priority {
+        return when (priority) {
+            "High Priority" -> {
+                Priority.HIGH
+            }
+            "Medium Priority" -> {
+                Priority.MEDIUM
+            }
+            "Low Priority" -> {
+                Priority.LOW
+            }
+            else -> {
+                Priority.LOW
+            }
+        }
+    }
+
+    private fun inputVerifyFromUser(
+        title: String,
+        description: String
+    ): Boolean {
+        return if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
+            false
+        } else !(title.isEmpty() || description.isEmpty())
+    }
+
+
 }
