e3c8c1449996feafa525c39262fd2984e175634c
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-08 10:39:01 +0530

    Recycler View is working and displaying the Data!

--- app/src/main/java/com/bharathkalyans/todolist/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/bharathkalyans/todolist/fragments/list/ListAdapter.kt
@@ -27,7 +27,9 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.itemView.title_txt.text = dataList[position].title
         holder.itemView.description_txt.text = dataList[position].description
+        holder.itemView.setOnClickListener {
 
+        }
 
         when (dataList[position].priority) {
             Priority.HIGH -> holder.itemView.priority_indicator.setCardBackgroundColor(
--- app/src/main/java/com/bharathkalyans/todolist/fragments/list/ListFragment.kt
+++ app/src/main/java/com/bharathkalyans/todolist/fragments/list/ListFragment.kt
@@ -3,28 +3,41 @@ package com.bharathkalyans.todolist.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bharathkalyans.todolist.R
+import com.bharathkalyans.todolist.data.viewmodel.ToDoViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
 
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val adapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
 
-        view.floatingActionButton2.setOnClickListener{
+
+        val recyclerView = view.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, { listOfToDoData ->
+            adapter.setData(listOfToDoData)
+        })
+
+
+        view.floatingActionButton2.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
 
-        view.listLayout.setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
-        }
+
 
 
         //Setting Menu
@@ -34,6 +47,6 @@ class ListFragment : Fragment() {
 
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.list_fragment_menu,menu)
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }
